44d83ff5a51186775499b50be374c99864cd79e4
Luis <luisangeldev@gmail.com>
2024-08-01 02:38:18 -0500

    Create CategorySeeder.php

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use PhpParser\Node\Stmt\Foreach_;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Fast Food',
+            'Cakes',
+            'Services',
+            'Plomeros',
+            'Carpinteros',
+            'Músicos',
+            'Mariachis',
+            'Eventos',
+        ];
+
+        foreach ($categories as $name) {
+            Category::factory()->create([
+                'name' => $name,
+            ]);
+        }
+    }
+}
