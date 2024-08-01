a7220559868a230a9283c9e9f23b597a2f7b3659
Luis <luisangeldev@gmail.com>
2024-08-01 18:15:57 -0500

    Adds delete action to categories table

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -55,6 +55,7 @@ class CategoryResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
