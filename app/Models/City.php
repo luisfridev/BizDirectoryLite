4fd9102cca304c612af75ee34f6d741072a1266b
Luis <luisangeldev@gmail.com>
2024-08-03 03:08:25 -0500

    Creates City model
    - Adds $fillable property
    - Adds HasSlug trait

---
+++ app/Models/City.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class City extends Model
+{
+    use HasFactory, HasSlug;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'is_active',
+        'is_intro',
+        'description',
+        'slug',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+}
