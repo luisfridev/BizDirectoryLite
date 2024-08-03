3ad2485595aceed9fb808cb835356810fea2aa17
Luis <luisangeldev@gmail.com>
2024-08-03 02:54:16 -0500

    Update 2024_07_31_230833_create_categories_table.php
    Changes column name from status to is_active

--- database/migrations/2024_07_31_230833_create_categories_table.php
+++ database/migrations/2024_07_31_230833_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('status')->nullable()->default(1);
+            $table->boolean('is_active')->nullable()->default(1);
             $table->string('intro', 160)->nullable();
             $table->text('description')->nullable();
             $table->string('slug')->nullable();
